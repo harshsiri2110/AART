482c6305e51b6191eb4aa9eb87f1fdaefe4439f7
Tanya Thakur <tanyathakur2000@gmail.com>
2020-09-14 18:25:03 +0530

    Test commit

--- app/src/main/java/com/example/aart/MainActivity.java
+++ app/src/main/java/com/example/aart/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public int yes;
-
+    public int no;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
