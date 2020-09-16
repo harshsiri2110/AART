e83622efe0f0277b54aa6d3e4e9dd9007988d3ff
Harsh Siriah <harsh.siriah17@vit.edu>
2020-09-16 17:17:18 +0530

    ImageSlider added

--- app/src/main/java/com/example/aart/Cards.java
+++ app/src/main/java/com/example/aart/Cards.java
@@ -3,6 +3,13 @@ package com.example.aart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cards extends AppCompatActivity {
 
@@ -11,5 +18,11 @@ public class Cards extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
 
+        ImageSlider imageSlider = findViewById(R.id.image);
+
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.dog,true));
+
+        imageSlider.setImageList(slideModels,true);
     }
 }
--- app/src/main/java/com/example/aart/MainActivity.java
+++ app/src/main/java/com/example/aart/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.aart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this,Cards.class));
     }
 }
