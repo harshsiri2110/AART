c539d12397591c8382112978aaf1a60d25e86bea
Harsh Siriah <harsh.siriah17@vit.edu>
2020-09-16 16:18:57 +0530

    ImageSlider added

---
+++ app/src/main/java/com/example/aart/Cards.java
@@ -0,0 +1,15 @@
+package com.example.aart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
+
+    }
+}
