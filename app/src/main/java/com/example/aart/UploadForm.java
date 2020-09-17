6c9ee89aef68f10b7763c0aee57397c0606a765b
Harsh Siriah <harsh.siriah17@vit.edu>
2020-09-17 17:27:59 +0530

    Upload form added

---
+++ app/src/main/java/com/example/aart/UploadForm.java
@@ -0,0 +1,14 @@
+package com.example.aart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_form);
+    }
+}
