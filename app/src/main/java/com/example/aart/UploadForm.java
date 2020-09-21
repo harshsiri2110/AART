75e56f519b09e4f7430843b8e4c41f736e5a7c24
Harsh Siriah <harsh.siriah17@vit.edu>
2020-09-21 15:54:26 +0530

    Radio button information fetch

--- app/src/main/java/com/example/aart/UploadForm.java
+++ app/src/main/java/com/example/aart/UploadForm.java
@@ -3,25 +3,51 @@ package com.example.aart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApiNotAvailableException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class UploadForm extends AppCompatActivity {
-EditText txttitle, txtage, locatn;
-Button btnupload;
-DatabaseReference reff;
+    EditText txttitle, locatn;
+    Button btnupload;
+    DatabaseReference reff;
+
+    String gender;
+
+    RadioGroup radioGenderGroup;
+    RadioButton selectedButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_form);
         txttitle=(EditText)findViewById(R.id.txttitle);
-        txtage=(EditText)findViewById(R.id.txtage);
         locatn= (EditText)findViewById(R.id.locatn);
+
+        radioGenderGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
         btnupload= (Button)findViewById(R.id.btnupload);
+
+        btnupload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId = radioGenderGroup.getCheckedRadioButtonId();
+
+                selectedButton = (RadioButton)findViewById(selectedId);
+
+                gender = selectedButton.getText().toString();
+            }
+        });
+
         reff= FirebaseDatabase.getInstance().getReference().child("Member");
+
     }
 }
