08d9f0dc398b8dfd6ae53f2abc2fc99e39150c1b
Tanya Thakur <tanyathakur2000@gmail.com>
2020-09-21 15:56:44 +0530

    Merge branch 'master' of C:\Users\TANYA\AndroidStudioProjects\AART1 with conflicts.

---
+++ app/src/main/java/com/example/aart/Member.java
@@ -0,0 +1,31 @@
+package com.example.aart;
+
+public class Member {
+    private String title;
+    private  String age;
+    private String locatn;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getLocatn() {
+        return locatn;
+    }
+
+    public void setLocatn(String locatn) {
+        this.locatn = locatn;
+    }
+}
--- app/src/main/java/com/example/aart/UploadForm.java
+++ app/src/main/java/com/example/aart/UploadForm.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApiNotAvailableException;
@@ -15,39 +13,30 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class UploadForm extends AppCompatActivity {
-    EditText txttitle, locatn;
-    Button btnupload;
-    DatabaseReference reff;
-
-    String gender;
-
-    RadioGroup radioGenderGroup;
-    RadioButton selectedButton;
-
-
+EditText txttitle, txtage, locatn;
+Button btnupload;
+DatabaseReference reff;
+Member member;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_form);
-        txttitle=(EditText)findViewById(R.id.txttitle);
-        locatn= (EditText)findViewById(R.id.locatn);
-
-        radioGenderGroup = (RadioGroup) findViewById(R.id.radioGroup);
-
-        btnupload= (Button)findViewById(R.id.btnupload);
-
-        btnupload.setOnClickListener(new View.OnClickListener() {
+        txttitle = (EditText)findViewById(R.id.txttitle);
+        txtage = (EditText)findViewById(R.id.txtage);
+        locatn = (EditText)findViewById(R.id.locatn);
+        btnupload = (Button)findViewById(R.id.btnupload);
+        member= new Member();
+        reff = FirebaseDatabase.getInstance().getReference().child("Member");
+        btnupload.setOnClickListener((new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                int selectedId = radioGenderGroup.getCheckedRadioButtonId();
+            public void onClick(View view){
+                member.setTitle(txttitle.getText().toString());
+                member.setAge(txtage.getText().toString().trim());
+                member.setLocatn(locatn.getText().toString().trim());
 
-                selectedButton = (RadioButton)findViewById(selectedId);
-
-                gender = selectedButton.getText().toString();
+                reff.push().setValue(member);
+                Toast.makeText(UploadForm.this, "data inserted");
             }
-        });
-
-        reff= FirebaseDatabase.getInstance().getReference().child("Member");
-
+        }));
     }
 }
