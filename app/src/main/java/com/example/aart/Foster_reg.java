8b98147045fd8e766b73a722ed2f567cee3b7f35
Harsh Siriah <harsh.siriah17@vit.edu>
2020-09-25 17:33:59 +0530

    Description added in database

--- app/src/main/java/com/example/aart/Foster_reg.java
+++ app/src/main/java/com/example/aart/Foster_reg.java
@@ -3,12 +3,27 @@ package com.example.aart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Foster_reg extends AppCompatActivity {
+
+    EditText name,number,email,password,confirmPassword;
+
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foster_reg);
+
+        name = findViewById(R.id.name);
+        number = findViewById(R.id.num);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.pass);
+        confirmPassword = findViewById(R.id.Cpass);
+
+        submit = findViewById(R.id.btnreg);
+
     }
 }
