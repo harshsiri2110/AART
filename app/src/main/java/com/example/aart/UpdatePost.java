bc274e820c365347ed2dcc442bf04c0a55e3872d
Harsh Siriah <harsh.siriah17@vit.edu>
2020-10-17 17:38:25 +0530

    Update Post

--- app/src/main/java/com/example/aart/UpdatePost.java
+++ app/src/main/java/com/example/aart/UpdatePost.java
@@ -1,15 +1,23 @@
 package com.example.aart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -17,10 +25,10 @@ public class UpdatePost extends AppCompatActivity {
 
     EditText txtTitle, txtAge, location, description, medical;
     Button btnupdate, selectImages;
-    DatabaseReference reff, reff2;
+    DatabaseReference reff;
     String species,title,gender,location_text,age,desc,medical_text,timestamp;
 
-    ArrayList<ImageUrl> imageList = new ArrayList<>();
+    ArrayList<SlideModel> imageList = new ArrayList<>();
 
     RadioButton male,female,dog,cat;
     ImageSlider imgView;
@@ -63,7 +71,47 @@ public class UpdatePost extends AppCompatActivity {
         txtAge.setText(age);
         location.setText(location_text);
 
+        if(species.equals("Dog"))
+        {
+            dog.toggle();
+        }
+        else
+        {
+            cat.toggle();
+        }
 
+        if(gender.equals("Male"))
+        {
+            male.toggle();
+        }
+        else
+        {
+            female.toggle();
+        }
+
+        reff = FirebaseDatabase.getInstance().getReference().child("Foster").child("Posts").child(timestamp).child("images");
+
+        reff.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot currImg : snapshot.getChildren())
+                {
+                    String uri = currImg.getValue(ImageUrl.class).getUri();
+                    imageList.add(new SlideModel(uri, ScaleTypes.CENTER_INSIDE));
+
+
+                    if(imageList.size() == snapshot.getChildrenCount())
+                    {
+                        imgView.setImageList(imageList,ScaleTypes.FIT);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
