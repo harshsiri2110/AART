8cbf80b684262a792546b3c573430d6eab4cbb0e
Harsh Siriah <harsh.siriah17@vit.edu>
2020-10-26 17:47:19 +0530

    Update Post

--- app/src/main/java/com/example/aart/Edit_Profile.java
+++ app/src/main/java/com/example/aart/Edit_Profile.java
@@ -3,12 +3,71 @@ package com.example.aart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Edit_Profile extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Edit_Profile extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton c1,c2,c3,c4,d1,d2,d3,d4;
+    CircleImageView profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__profile);
+        profilePic = findViewById(R.id.edit_profile_photo);
+
+        c1 = (ImageButton) findViewById(R.id.cat_profile1);
+        c2 = (ImageButton) findViewById(R.id.cat_profile2);
+        c3 = (ImageButton) findViewById(R.id.cat_profile3);
+        c4 = (ImageButton) findViewById(R.id.cat_profile4);
+        d1 = (ImageButton) findViewById(R.id.dog_profile1);
+        d2 = (ImageButton) findViewById(R.id.dog_profile2);
+        d2 = (ImageButton) findViewById(R.id.dog_profile3);
+        d2 = (ImageButton) findViewById(R.id.dog_profile4);
+
+        c1.setOnClickListener(this);
+        c2.setOnClickListener(this);
+        c3.setOnClickListener(this);
+        c4.setOnClickListener(this);
+        d1.setOnClickListener(this);
+        d2.setOnClickListener(this);
+        d3.setOnClickListener(this);
+        d4.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId())
+        {
+            case R.id.cat_profile1:
+                profilePic.setImageResource(R.drawable.c1);
+                break;
+            case R.id.cat_profile2:
+                profilePic.setImageResource(R.drawable.c2);
+                break;
+            case R.id.cat_profile3:
+                profilePic.setImageResource(R.drawable.c3);
+                break;
+            case R.id.cat_profile4:
+                profilePic.setImageResource(R.drawable.c4);
+                break;
+            case R.id.dog_profile1:
+                profilePic.setImageResource(R.drawable.d1);
+                break;
+            case R.id.dog_profile2:
+                profilePic.setImageResource(R.drawable.d2);
+                break;
+            case R.id.dog_profile3:
+                profilePic.setImageResource(R.drawable.d3);
+                break;
+            case R.id.dog_profile4:
+                profilePic.setImageResource(R.drawable.d4);
+                break;
+        }
     }
 }
