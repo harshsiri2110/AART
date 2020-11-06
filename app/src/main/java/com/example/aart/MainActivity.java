f286a2bc65566bdb2e67da6292e81700f329a073
Tanya Thakur <tanyathakur2000@gmail.com>
2020-11-06 15:51:04 +0530

    filter

--- app/src/main/java/com/example/aart/MainActivity.java
+++ app/src/main/java/com/example/aart/MainActivity.java
@@ -80,6 +80,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Listview for cards
         listView = (ListView) findViewById(R.id.listView);
