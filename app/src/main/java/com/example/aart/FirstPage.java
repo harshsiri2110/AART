68ea298471f2ce09ef045a19298168f05f9eecad
Tanya Thakur <tanyathakur2000@gmail.com>
2021-01-14 17:13:29 +0530

    about us gif

--- app/src/main/java/com/example/aart/FirstPage.java
+++ app/src/main/java/com/example/aart/FirstPage.java
@@ -145,7 +145,7 @@ public class FirstPage extends AppCompatActivity {
                 rotateAnimation.setRepeatCount(0);
                 logo.startAnimation(rotateAnimation);
 
-                Glide.with(FirstPage.this).load(R.drawable.about_us_gif).into(aboutUs);
+                Glide.with(FirstPage.this).load(R.drawable.about_us_gif_foo).into(aboutUs);
 
                 handler.postDelayed(new Runnable() {
                     @Override
